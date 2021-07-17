195580811962c7161d849037e7cca01002bc672d
Alex Mason <apmason119@gmail.com>
2021-07-17 19:55:37 -0400

    Add error message

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,11 +7,56 @@
 
 import SwiftUI
 
+struct AlertState {
+    var showAlert = false
+    private var errorCode: Int?
+    
+    mutating func showAlert(_ errorCode: Int?) {
+        self.showAlert = true
+        self.errorCode = errorCode
+    }
+    
+    mutating func reset() {
+        self.showAlert = false
+        self.errorCode = nil
+    }
+    
+    var errorString: String {
+        if let errorCode = errorCode {
+            return "Error code: \(errorCode)"
+        } else {
+            return "Network error"
+        }
+    }
+}
+
 struct ContentView: View {
+    @State var alertState = AlertState()
+    
     var body: some View {
         Button("Log In With Twitter") {
-            TwitterSignIn.shared.signIn()
+            TwitterSignIn.shared.signIn { result in
+                switch result {
+                case .success:
+                    // to new view
+                    break
+                    
+                case .failure(let error):
+                    switch error {
+                    case .networkError:
+                        self.alertState.showAlert(nil)
+                        
+                    case .apiError(statusCode: let code):
+                        self.alertState.showAlert(code)
+                    }
+                }
+            }
         }
+        .alert(isPresented: $alertState.showAlert, content: {
+            Alert.init(title: Text("Error"), message: Text(alertState.errorString), dismissButton: .cancel(Text("Okay"), action: {
+                self.alertState.reset()
+            }))
+        })
         .padding(.all)
     }
 }
--- Shared/Models/TwitterSignIn.swift
+++ Shared/Models/TwitterSignIn.swift
@@ -9,6 +9,11 @@ import Swifter
 import Foundation
 import AuthenticationServices
 
+enum TwitterError: Error {
+    case apiError(statusCode: Int)
+    case networkError
+}
+
 class TwitterSignIn: NSObject {
     
     private var swifter: Swifter?
@@ -18,15 +23,19 @@ class TwitterSignIn: NSObject {
     
     private override init() {}
     
-    func signIn() {
+    func signIn(completion: @escaping (Result<Void, TwitterError>) -> Void) {
         swifter = Swifter(consumerKey: Constants.consumerKey, consumerSecret: Constants.consumerSecret)
         swifter?.authorize(withProvider: self, callbackURL: URL(string: Constants.callbackURL)!) { token, response in
-            guard let httpResponse = response as? HTTPURLResponse else { return }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.networkError))
+                return
+            }
+            
             if httpResponse.statusCode == 200 {
                 self.token = token
-                
+                completion(.success(()))
             } else {
-                // TODO: Present error to user
+                completion(.failure(.apiError(statusCode: httpResponse.statusCode)))
             }
         }
     }
