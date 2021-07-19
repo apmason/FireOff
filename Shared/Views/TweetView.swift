e6434583d80b5f6fa4c58dec77035cb3d071e3a3
Alex Mason <apmason119@gmail.com>
2021-07-19 15:16:38 -0400

    Updating SwiftUI view

--- Shared/Views/TweetView.swift
+++ Shared/Views/TweetView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct TweetView: View {
+    
+    @State private var tweetText: String = ""
+    
     var body: some View {
-        VStack {
-            Text(TwitterSignIn.shared.userName ?? "Loading")
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 20) {
+                HStack {
+                    Text(TwitterSignIn.shared.userName ?? "Loading")
+                    Text("FUCK")
+                }.frame(width: .infinity, alignment: .trailing)
+                
+                TextField("Fire it Off!", text: $tweetText)
+                    .frame(maxHeight: .infinity, alignment: .topLeading)
+                Divider()
+                Text("240")
+                    .frame(alignment: .trailing)
+                
+            }
+            Text("Hey brother")
         }
+        .padding(20)
     }
 }
 
 struct TweetView_Previews: PreviewProvider {
+    @State static var tweetText: String = "Hey!"
+    
     static var previews: some View {
         TweetView()
     }
