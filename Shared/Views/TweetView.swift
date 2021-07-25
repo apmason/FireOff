ce00a105df1dca4ac5ae701fcfad572e7496b5bb
apmason <apmason119@gmail.com>
2021-07-25 11:24:03 -0400

    Fix, but nothing to do?

--- Shared/Views/TweetView.swift
+++ Shared/Views/TweetView.swift
@@ -18,55 +18,14 @@ struct TweetView: View {
     private var twitterModel: TwitterSignIn = TwitterSignIn.shared
     
     var body: some View {
-        VStack(alignment: .center) {
-            HStack(alignment: .center) {
-                Image(systemName: "gear")
-                    .resizable()
-                    .frame(width: 30, height: 30, alignment: .leading)
-                    .padding(15)
-                Spacer()
-                Button("Tweet") {
-                    guard !sendingTweet else { return } // prevent double taps
-                    sendingTweet = true
-                    twitterModel.sendTweet(tweetText) { error in
-                        sendingTweet = false
-                        // Close out alert
-                        guard let error = error else {
-                            tweetText = "" // reset the TextEditor
-                            // pop an alert up here showing success for a few seconds.
-                            return
-                        }
-                        errorAlert.showAlert(for: error)
-                    }
-                }
-                .disabled(remainingCharacters < 0 || remainingCharacters == 240)
-                .frame(height: 30, alignment: .trailing)
-                .padding(15)
+        HStack(alignment: .top, spacing: 10, content: {
+            Color.gray
+                .frame(width: 30, height: 30)
                 .cornerRadius(15)
-            }
-            .padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            
-            HStack(alignment: .top, spacing: 10, content: {
-                ProfileImage()
-                    .frame(width: 30, height: 30)
-                    .cornerRadius(15)
-                
-                //Text("Hello?")
-                
-                ZStack(alignment: .topLeading) {
-                    PlaceholderEditor(text: $tweetText, placeholderText: "FireOff a tweet!", onChange: {
-                        remainingCharacters = maxCharacters - tweetText.count
-                    })
-                    .frame(alignment: .topLeading)
-                    //                        .frame(width: 100,
-                    //                               height: 100,
-                    //                               alignment: .topLeading)
-                    Color.blue.opacity(0.3)
-                }
-            })
-            .padding(.horizontal, 10)
-            Spacer()
-        }
+            Color.blue
+        })
+        .padding(.horizontal, 20)
+        
     }
 }
 
